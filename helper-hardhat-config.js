073e2fab8f1293d850a06dcaf0e1fcb63392ce2f
Malebu <busch.marius@web.de>
2022-08-26 17:34:09 +0200

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,27 +1,16 @@
+// to set addresses for different networks
 
-/*
-function deployFunc() { //as a default deplyo func
-    console.log("Hi!")
+const networkconfig = {
+    4: {
+        name: "rinkeby",
+        ethUsdPriceFeed: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
+    },
+    137: {
+        name: "polygon",
+        ethUsdPRiceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945",
+    },
 }
 
-module.exports.default = deployFunc()
-*/
-
-const { networkConfig } = require("../helper-hardhat-config")
-//shorter&easier way
-
-module.exports = async (hre) => { //deploy automatic
-    const { getNamedAccounts, deployments } = hre //get variables
-    const {deploy, log } = deployments
-    const {deployer } = await getNamedAccounts()
-    const chainId = network.config.chainId
-
-    const ethUsdPRiceFeedAddress = networkConfig[chainId]["ethUsdpriceFeed"]
-    //for mock contracts
-
-    const fundMe = await deploy("FundMe", { //deploys contract
-        from: deployer,
-        args: [/* address*/], // put price feedaddress in here
-        log: true,
-    })
+module.exports = {
+    networkConfig,
 }
